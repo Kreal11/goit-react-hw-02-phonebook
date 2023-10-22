72e7cd86880b00af8dec8d41f3467dc62ca0a04a
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-23 00:28:11 +0300

    remove prop-types from app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -92,17 +92,3 @@ export class App extends Component {
     );
   }
 }
-
-App.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  filter: PropTypes.string,
-  handlChangeFilter: PropTypes.func,
-  handleAddNewContact: PropTypes.func,
-  handleDeleteContact: PropTypes.func,
-};
